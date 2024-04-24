d66223d9149c4ba55b53cb7be32cb1cb9495a1f4
kendpr <real.kendpr@gmail.com>
2024-04-24 12:52:27 +0800

    Configure fonts and colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Figtree", "sans-serif"],
+      },
+      colors: {
+        primary: "hsl(47, 88%, 63%)",
+        white: "hsl(0, 0%, 100%)",
+        black: "hsl(0, 0%, 7%)",
+        grey: "hsl(0, 0%, 50%)",
+      },
+      boxShadow: {
+        solid: " 8px 8px 0px",
+      },
+    },
   },
   plugins: [],
 };
